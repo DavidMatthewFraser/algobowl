ac816243ebf8296fb5355658c3120ca86eb27cbb
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-02-17 11:55:03 -0700

    added recursive solution that uses unordered_sets:w

---
+++ algbwl.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+unordered_set<int> seen = {1};
+unordered_set<int> calculated;
+vector<pair<int, int>> sequence;
+
+void getSequence(int n){
+    if(seen.find(n/2) != seen.end()) { // if we can use n/2
+        if(calculated.find(n/2) == calculated.end()) { // if we have not already calculated n/2 + n/2
+            sequence.push_back(pair<int, int>(n / 2, n / 2));
+            calculated.insert(n/2);
+            seen.insert(2*(n/2));
+        }
+        if(n % 2) {
+            sequence.push_back(pair<int, int>(n - 1, 1));
+            seen.insert(n);
+        }
+    } else {
+        getSequence(n/2);
+        getSequence(n);
+    }
+}
+
+int main() {
+    auto start = high_resolution_clock::now();
+
+    /*********************************************/
+    // int input[] = {2, 5};
+    // int input[] = {2, 5, 10, 15, 16, 24, 25};
+    int input[] = {3, 5, 9, 11, 13, 15, 17, 21, 31, 45, 55, 121};
+    for(int i : input)
+        getSequence(i);
+    cout << sequence.size() << endl;
+    for(pair<int, int> p : sequence) 
+        cout << p.first << " " << p.second << endl;
+    /*********************************************/
+
+    auto stop = high_resolution_clock::now(); 
+    cout << duration_cast<microseconds>(stop - start).count() <<  " microseconds" << endl;
+    return 0;
+}
