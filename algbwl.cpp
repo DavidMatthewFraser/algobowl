665dcc6ee5041847575899ae4d5fdf0907698b70
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-02-23 11:01:17 -0700

    some more ideas for inputs

--- algbwl.cpp
+++ algbwl.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 #include <vector>
 #include <unordered_set>
 #include <fstream>
@@ -11,7 +11,7 @@ unordered_set<int> seen = {1};
 unordered_set<int> calculated;
 vector<pair<int, int>> sequence;
 
-void getSequence(int n){
+void getSequence(int n) {
     if(seen.find(n/2) != seen.end()) { // if we can use n/2
         if(calculated.find(n/2) == calculated.end()) { // if we have not already calculated n/2 + n/2
             sequence.push_back(pair<int, int>(n / 2, n / 2));
@@ -33,23 +33,31 @@ int main() {
     ifstream infile("input.txt");
     int num;
     vector<int> input;
-    while(infile >> num) {
-        input.push_back(num);
-    }
-    // int input[] = {2, 5};
-    // int input[] = {2, 5, 10, 15, 16, 24, 25};
-    // int input[] = {2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811};
-    // int input[] = {2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296};
+    while(infile >> num) input.push_back(num);
     auto start = high_resolution_clock::now();
     /*********************************************/
-        for(int i : input)
-        getSequence(i);
+    for(int i : input) getSequence(i);
     cout << sequence.size() << endl;
-    for(pair<int, int> p : sequence) 
-        cout << p.first << " " << p.second << endl;
+    for(pair<int, int> p : sequence) cout << p.first << " " << p.second << endl;
     /*********************************************/
     auto stop = high_resolution_clock::now(); 
     cout << duration_cast<microseconds>(stop - start).count() <<  " microseconds" << endl;
-    infile.close();
+    //infile.close();
     return 0;
 }
+
+// Some Ideas for test input
+
+// int input[] = {2, 5};
+// int input[] = {2, 5, 10, 15, 16, 24, 25};
+// int input[] = {2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811};
+// int input[] = {2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296};
+
+// using the max number of inputs and the largest possible numbers:
+/*
+    vector<int> input;
+    int maxInput = 1000000000;
+    int maxInputs = 1000;
+    for(int i=0; i<maxInputs; i++) input.push_back(maxInput-i);
+    
+*/
